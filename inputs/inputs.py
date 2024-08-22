11f5820be121a4608c3ca404c4733fbbedd89b58
HECTOR EDUARDO ASTUDILLO <91333128+calvo2244@users.noreply.github.com>
2024-08-21 19:30:57 -0500

    desplegamos inputs

---
+++ inputs/inputs.py
@@ -0,0 +1,5 @@
+# pedirle un dato a el usuario 
+nombre  = input("cual es su nombre:  ")
+# mostrando el dato 
+print(f'el nombre es {nombre}')
+print(type(nombre))
---
+++ inputs/inputs_numeros.py
@@ -0,0 +1,11 @@
+# pedirle un numero a el usuario 
+numero  = input("Dame un numero campista:  ")
+# multiplcando el numero por 2
+resultado = int(numero) * 2
+print(resultado)
+print(type(resultado))
+
+# convirtiendo el numero entero a float 
+float = float(numero) * 2
+print("respuesta float", float)
+print(type(resultado))
