79e410710954431263cd68ac6b65067b193cfda4
HECTOR EDUARDO ASTUDILLO <91333128+calvo2244@users.noreply.github.com>
2024-08-26 18:51:24 -0500

    bucles y control de iteraciones

---
+++ bucles_y_control_de_iteraciones/bucles_y_control_de_iteraciones.py
@@ -0,0 +1,31 @@
+numbers= [1,2,3,4,5,6]
+
+for i in numbers:
+    print("aqui i es igual a: ", i+1)
+
+for i in range(10):
+    print("range 0 hasta 10  ",i)
+    
+for i in range(3,10):
+    print("range desde a hasta b",i)
+
+fruits = ["Manzana","Pera","Uva","Naranja","Tomate","kiwi"]    
+for fruta in fruits:
+    print("lista de frutas",fruta)
+    if (fruta== "Naranja" ):
+        print("Naranja encontrada")
+    
+x = 0
+while x<5:
+    if x == 3:
+        break
+    print(x)
+    x += 1
+    
+numbers= [1,2,3,4,5,6]
+    
+for i in numbers:
+    if i == 3:
+        break
+    print("aqui i es igual a: ", i+1)
+
